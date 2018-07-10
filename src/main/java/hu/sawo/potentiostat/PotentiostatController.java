d27023a41303ac9f7b1985e7e72c1a70a8981cfc
sawo <sanyi@inlineskate.hu>
2018-07-10 18:42:12 +0200

    Init is automatic, serial port is not necessary to provide

--- src/main/java/hu/sawo/potentiostat/PotentiostatController.java
+++ src/main/java/hu/sawo/potentiostat/PotentiostatController.java
@@ -5,7 +5,6 @@ import hu.sawo.potentiostat.instructions.Instruction;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
-import jssc.SerialPortTimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static com.google.common.collect.Lists.newArrayList;
 
@@ -27,35 +27,49 @@ public class PotentiostatController {
 
     private final Logger log = LoggerFactory.getLogger(PotentiostatController.class);
 
+    private SerialPort serialPort;
+
+    @PostConstruct
+    public void initPotentiostat() {
+        final Optional<String> hopefullyGoodPort = getSerialPorts().stream().findFirst();
+        hopefullyGoodPort.ifPresent(serialPortName -> {
+            serialPort = new SerialPort(serialPortName);
+            try {
+                log.debug("Opening port [{}] ...", serialPortName);
+                serialPort.openPort();
+                log.debug("Successful");
+                serialPort.setParams(SerialPort.BAUDRATE_9600,
+                                     SerialPort.DATABITS_8,
+                                     SerialPort.STOPBITS_1,
+                                     SerialPort.PARITY_NONE);
+                // setting up resolution
+                serialPort.writeString("resolution2@,");
+
+            } catch (SerialPortException e) {
+                log.error(e.getMessage());
+                System.exit(-1);
+            }
+
+        });
+    }
+
     @GetMapping("/serial")
     public List<String> getSerialPorts() {
         return newArrayList(SerialPortList.getPortNames());
     }
 
     @GetMapping("simulation")
-    public List<CV> getResults(@RequestParam("resolution") String resolution,
-                               @RequestParam("startVolt") float startVolt,
+    public List<CV> getResults(@RequestParam("startVolt") float startVolt,
                                @RequestParam("peakVolt") float peakVolt,
                                @RequestParam("scanRate") int scanRate,
-                               @RequestParam("waveType") int waveType,
-                               @RequestParam("serial") String serialPortName) throws SerialPortException,
-                                                                                     InterruptedException,
-                                                                                     SerialPortTimeoutException,
-                                                                                     IOException {
+                               @RequestParam("waveType") int waveType) throws SerialPortException,
+                                                                              InterruptedException {
+        if (serialPort == null) {
+            log.error("OliView was not found");
+            return newArrayList();
+        }
+        
         List<CV> result = newArrayList();
-        SerialPort serialPort = new SerialPort(serialPortName);
-        log.debug("Opening port [{}] ...", serialPortName);
-        serialPort.openPort();
-        log.debug("Successful");
-
-        serialPort.setParams(SerialPort.BAUDRATE_9600,
-                             SerialPort.DATABITS_8,
-                             SerialPort.STOPBITS_1,
-                             SerialPort.PARITY_NONE);
-
-        // setting up resolution
-        serialPort.writeString(resolution + "@,");
-        Thread.sleep(6000);
         // sending command
         String inputData =
             new CyclicVoltametry(startVolt, peakVolt, scanRate, Instruction.WaveType.values()[waveType]).toString();
